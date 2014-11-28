a421964960d903497bfe8249e207057bac056c65
LDalby <lars@bios.au.dk>
2014-11-28 09:31:29 +0100

    Add R script to generate the farmref file

---
+++ MakingTheFarmref.R
@@ -0,0 +1,28 @@
+# Making the farmref file
+# Date: 28-11-2014
+# Author: Lars Dalby
+
+if(!require(ralmass)) 
+{
+	library(devtools)
+	install_github('LDalby/ralmass')
+}
+library(ralmass)
+library(data.table)
+
+# ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ #
+
+farm = fread('o:/ST_LandskabsGenerering/outputs/Houlbjerg/FarmInfo2013.txt')  # See ???? for documentation of the steps from raw data to this file...
+
+The2013Farmref = farm[, c('BedriftID', 'FarmType'), with = FALSE]
+dim(The2013Farmref)  #  619112      2
+The2013Farmref = unique(The2013Farmref)
+dim(The2013Farmref)  # 38812     2
+The2013Farmref[,BedriftID:= gsub(pattern = ',', replacement = '', x = The2013Farmref$BedriftID, fixed = FALSE)]  # Fixing the comma sep issue
+The2013Farmref[,BedriftID:= as.numeric(The2013Farmref$BedriftID)]  # Potentially risky...
+setkey(The2013Farmref, 'BedriftID')
+
+path = paste('o:/ST_LandskabsGenerering/outputs/The2013Farmref ', Sys.Date(),'.txt', sep = '')
+WritePolyref(Table = The2013Farmref, PathToFile = path)  #See ?WritePolyref for documentation.
+
+# ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ #
